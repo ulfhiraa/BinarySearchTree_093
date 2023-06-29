ca216635846777cf4c0ebfda99643b2421c9cf61
ulfhiraa <studyiraa@gmail.com>
2023-06-29 17:49:12 +0700

    terdapat 3 langkah dalam search operation

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -30,6 +30,17 @@ public:
 
 	void search(string element, Node*& parent, Node*& currentNode)
 	{
+		// This function searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT; // step 1. make a variable/pointer currentNode point to the ROOT node
+		parent = NULL; // step 2. Make a variable/pointer parent point to NULL
 
+		while ((currentNode != NULL) && (currentNode->info != element)) // step 3. repeats a, b, c until currentNode =NULL or node to be searches = currentNode
+		{
+			parent = currentNode;// 3a
+			if (element < currentNode->info) // 3b
+				currentNode = currentNode->leftChild;
+			else // 3c
+				currentNode = currentNode->rightChild;
+		}
 	}
 };
